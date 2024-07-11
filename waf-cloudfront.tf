febb99c4769f6f89f78707f11f42b58004ec133c
Maycon Costa <maycon.costa@dnx.solutions>
2024-07-12 09:14:18 +1000

    Update ip_set_reference_statement

--- waf-cloudfront.tf
+++ waf-cloudfront.tf
@@ -186,14 +186,15 @@ resource "aws_wafv2_web_acl" "waf_cloudfront" {
       }
 
       statement {
-        dynamic "ip_set_reference_statement" {
-          for_each = rule.value.ip_set_reference_statement != {} ? [rule.value.ip_set_reference_statement] : []
-          content {
-            arn = rule.value.ip_set_arn
-            ip_set_forwarded_ip_config {
-              fallback_behavior = ip_set_reference_statement.value.fallback_behavior
-              header_name       = ip_set_reference_statement.value.header_name
-              position          = ip_set_reference_statement.value.position
+        ip_set_reference_statement {
+          arn =  length(try(rule.value.ip_set, [])) > 0 ? aws_wafv2_ip_set.ip_set[rule.value.name].arn : rule.value.ip_set_arn
+
+          dynamic "ip_set_forwarded_ip_config" {
+            for_each = rule.value.ip_set_reference_statement != null ? [1] : []
+            content {
+              fallback_behavior = rule.value.ip_set_reference_statement.fallback_behavior
+              header_name       = rule.value.ip_set_reference_statement.header_name
+              position          = rule.value.ip_set_reference_statement.position
             }
           }
         }
